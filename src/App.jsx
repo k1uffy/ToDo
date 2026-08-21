b35d45c088bfff5359c08bf47d4ac06c9c16c3ea
Alyona <fedalena417@gmail.com>
2026-08-21 17:42:54 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App() {
+  return (
+      <div className="app">
+          <h1>Список дел на {new Date().toLocaleDateString()} : Ничего</h1>
+      </div>
+  )
+}
+
+export default App
